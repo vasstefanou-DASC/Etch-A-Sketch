af6a99c3cce1f60ec576557e8a3ee1af26b701ef
Vasileios Stefanou <vas.stefanou@gmail.com>
2026-03-10 13:26:19 +0200

    Add non-functional buttons

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,3 +1,17 @@
 const projectName = document.createElement("h1");
+projectName.classList.add("parts");
 projectName.textContent = "Etch-A-Sketch";
 document.body.appendChild(projectName);
+
+const buttonsArea = document.createElement("div");
+buttonsArea.classList.add("parts");
+const buttonNames = ["Choose your grid","Same Color","Random Color","Opacity Mode"];
+for (let i=0;i<4;i++) {
+    const button = document.createElement("button");
+    button.textContent = buttonNames[i];
+    buttonsArea.appendChild(button);
+}
+document.body.appendChild(buttonsArea);
+
+
+

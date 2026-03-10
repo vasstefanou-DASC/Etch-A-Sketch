c11230181f1220c81c1a153ed0f6f5aa84f1b0c7
Vasileios Stefanou <vas.stefanou@gmail.com>
2026-03-10 16:51:52 +0200

    Add whole project but no extra credit

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -16,16 +16,37 @@ document.body.appendChild(buttonsArea);
 const sketchContainer = document.createElement("div");
 sketchContainer.classList.add("container");
 function createBoxes(boxesSide) {
+    deleteBoxes();
     const size = 480/boxesSide;
     for (let i=0;i<boxesSide**2;i++) {
         const sketchBox = document.createElement("div");
         sketchBox.classList.add("box");
         sketchBox.style.width = `${size}px`;
         sketchBox.style.height = `${size}px`;
+        sketchBox.addEventListener('mouseenter', () => {
+            sketchBox.style.backgroundColor = "hsla(182,100%,50%)";
+        });
         sketchContainer.appendChild(sketchBox);
     }
 }
-createBoxes(16);    
+createBoxes(16);
 document.body.appendChild(sketchContainer);
 
+function deleteBoxes() {
+    while (sketchContainer.lastElementChild) {
+        sketchContainer.removeChild(sketchContainer.lastElementChild);
+    }
+}
+
+const newGridButton = document.querySelectorAll("button").item(0);
+newGridButton.addEventListener("click", () => {
+    while(true) {
+        let gridSize = +prompt("What will be the new grid size (Maximum 100)?: ");
+        if (gridSize <= 100 && gridSize > 0) {
+            createBoxes(gridSize);
+            break;
+        }
+        alert("Please enter a number between 1 and 100");
+    }
+});
 
